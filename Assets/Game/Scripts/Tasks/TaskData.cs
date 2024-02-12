bdb50a5ba7b79622b136cd1c2a1bcecfdf6bc8eb
Helnyr <badia.noguero@gmail.com>
2024-02-12 02:51:11 +0100

    items de tareas spawn aleatorio

--- Assets/Game/Scripts/Tasks/TaskData.cs
+++ Assets/Game/Scripts/Tasks/TaskData.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Serialization;
+using System.Collections.Generic;
 
 [CreateAssetMenu]
 public class TaskData : ScriptableObject
@@ -7,4 +8,5 @@ public class TaskData : ScriptableObject
     public string TaskDescription;
     public int AmountToComplete;
     public Item.ItemType ObjectiveItemType;
+    public List<GameObject> requiredItem = new List<GameObject>();
 }
--- Assets/Game/Scripts/Tasks/TaskManager.cs
+++ Assets/Game/Scripts/Tasks/TaskManager.cs
@@ -18,9 +18,12 @@ public class TaskManager : MonoBehaviour
 
     public int CurrentTasksIndex = 0;
     [SerializeField] private Transform displayTransform;
+    [SerializeField] private Transform itemSpawner;
+
+    [SerializeField]private List<GameObject> requiredTaskItems = new List<GameObject>();
 
     public bool AllTasksDone => currentTasksList.All(x => x.TaskCompleted);
-    
+
     private void Awake()
     {
         Instance = this;
@@ -43,8 +46,11 @@ public class TaskManager : MonoBehaviour
             TaskInfoUI taskInfoUI = Instantiate(taskInfoUIPrefab, displayTransform);
             taskInfoUI.SetDescription(taskData.TaskDescription);
             taskInfoUI.SetCheckmark(false);
+            GenerateTaskItemList(taskData);
             currentTasksList.Add(new Task(taskData, taskInfoUI));
         }
+
+        SpawnRandomizedTaskItem();
     }
 
     public void TaksObjectiveDone(Item.ItemType taskType)
@@ -55,6 +61,33 @@ public class TaskManager : MonoBehaviour
                 return;
 
             task.ObjectiveDone();
+        }
+    }
+
+    public void GenerateTaskItemList(TaskData data)
+    {
+        if (data.requiredItem == null) return;
+        for (int i = 0; i < data.requiredItem.Count; i++)
+        {
+            requiredTaskItems.Add(data.requiredItem[i]);
+        }
+    }
+
+    public void SpawnRandomizedTaskItem()
+    {
+        System.Random random = new System.Random();
+
+        for (int i = requiredTaskItems.Count - 1; i > 0; i--)
+        {
+            int n = random.Next(i + 1);
+            GameObject item = requiredTaskItems[n];
+            requiredTaskItems[n] = requiredTaskItems[i];
+            requiredTaskItems[i] = item;
+        }
+
+        for (int i = 0; i < requiredTaskItems.Count; i++)
+        {
+            Instantiate(requiredTaskItems[i], itemSpawner.position, itemSpawner.rotation);
         }
     }
 }
